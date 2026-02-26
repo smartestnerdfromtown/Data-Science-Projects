dc80fae98fb0f2c446e6a6f23277373ad86ececd
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-02-26 14:00:17 +0400

    training polynomial regression

--- Students Performance/models.py
+++ Students Performance/models.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 RANDOM_STATE = 777
 
@@ -15,6 +17,18 @@ def split_data(df: pd.DataFrame, test_size: float):
 
     return X_train, X_test, y_train, y_test
 
+def train_polynomial_regression(degree: int, X_train, X_test, y_train):
+    polynomial_regression = PolynomialFeatures(degree=degree)
+
+    X_train_poly = polynomial_regression.fit_transform(X_train)
+    X_test_poly = polynomial_regression.transform(X_test)
+
+    linear_regression = LinearRegression()
+    linear_regression.fit(X_train_poly, y_train)
+
+    return linear_regression, X_test_poly
+
+
 
 
 
@@ -23,6 +37,24 @@ def main():
     df = pd.read_csv(filepath_or_buffer="student_performance_cleaned.csv")
 
     X_train, X_test, y_train, y_test = split_data(df=df, test_size=0.2)
+
+    polynomial_regression, X_test_poly = train_polynomial_regression(
+        degree=3, 
+        X_train=X_train, 
+        X_test=X_test,
+        y_train=y_train
+    )
+
+    y_pred = polynomial_regression.predict(X_test_poly)
+
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print("MSE:", mse)
+    print("R2:", r2)
+
+    print("Coefficients:", polynomial_regression.coef_)
+    print("Intercept:", polynomial_regression.intercept_)
 
 
 
