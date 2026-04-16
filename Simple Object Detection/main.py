19058b403b4bd98232e7a99b5ad144aa1cb7eda8
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-16 10:02:48 +0400

    simple object detection. initial commit

---
+++ Simple Object Detection/main.py
@@ -0,0 +1,91 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+import matplotlib.pyplot as plt
+
+def detect_objects(image_path: str):
+    """
+    Detect objects in an image using YOLOv8.
+    
+    Args:
+        image_path: Path to the input image
+    
+    Returns:
+        Detected objects and class labels.
+    """
+    model = YOLO("yolov8n.pt")
+
+    image = cv2.imread(filename=image_path)
+    image_rgb = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2RGB)
+
+    results = model(image_rgb)[0]
+
+    annotated_image = image_rgb.copy()
+    
+    colors = np.random.randint(0, 255, size=(100, 3), dtype=np.uint8)
+    
+    boxes = results.boxes 
+
+    return boxes, results.names, annotated_image, colors
+
+def show_results(image_path: str, confidence_threshold: float):
+    """
+    Show original image and detection results side by side.
+
+    Args:
+        image_path: Path to the input image
+        confidence_threshold: Minimum confidence score for detections
+    """
+    original_image = cv2.imread(filename=image_path)
+    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
+    boxes, class_names, annotated_image, colors = detect_objects(image_path=image_path)
+
+    class_labels = {}
+    for box in boxes:
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+        confidence = float(box.conf[0])
+
+        if confidence > confidence_threshold:
+            class_id = int(box.cls[0])
+            class_name = class_names[class_id]
+
+            color = colors[class_id % len(colors)].tolist()
+
+            cv2.rectangle(
+                img=annotated_image, 
+                pt1=(x1, y1), 
+                pt2=(x2, y2), 
+                color=color, 
+                thickness=2
+            )
+
+            class_labels[class_name] = color
+    
+    plt.figure(figsize=(15, 7))
+    
+    plt.subplot(1, 2, 1)
+    plt.title('Original Image')
+    plt.imshow(original_image)
+    plt.axis('off')
+    
+    plt.subplot(1, 2, 2)
+    plt.title('Detected Objects')
+    plt.imshow(annotated_image)
+    plt.axis('off')
+
+    legend_handles = []
+    for class_name, color in class_labels.items():
+        normalized_color = np.array(color) / 255.0 
+        legend_handles.append(plt.Line2D([0], [0], marker='o', color='w', label=class_name,
+                                           markerfacecolor=normalized_color, markersize=10))
+
+    plt.legend(handles=legend_handles, loc='upper right', title='Classes')
+
+    plt.tight_layout()
+    plt.show()
+
+
+print(detect_objects(image_path="guy_and_cats.jpg"))
+show_results(image_path="guy_and_cats.jpg", confidence_threshold=0.3)
