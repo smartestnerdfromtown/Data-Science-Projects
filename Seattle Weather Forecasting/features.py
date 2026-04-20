80db66beefae47067b9c9dc505c176de9439ec1b
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-20 07:53:46 +0400

    modular development

--- Seattle Weather Forecasting/features.py
+++ Seattle Weather Forecasting/features.py
@@ -1,7 +1,24 @@
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+
 def extract_features(df: pd.DataFrame):
     num_features = df.select_dtypes(include=np.number).columns.tolist()
     cat_features = df.select_dtypes(include=["object", "str"]).columns.tolist()
     return num_features, cat_features
 
+def split_data(
+        X: pd.DataFrame, 
+        y: pd.Series, 
+        random_state: int = 777, 
+        test_size: float = 0.2
+    ):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=y
+    )
+
+    return X_train, X_test, y_train, y_test
--- Seattle Weather Forecasting/models/extreme_boosting.py
+++ Seattle Weather Forecasting/models/extreme_boosting.py
@@ -1,9 +1,39 @@
 import pandas as pd
 import numpy as np
 
+from features import extract_features, split_data
+
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import RandomizedSearchCV
+
+
+def gradient_boosting_pipeline(model, num_features: list, cat_features: list):
+    num_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="median"))
+    ])
+
+    cat_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("encoder", OneHotEncoder(handle_unknown="ignore"))
+    ])
+
+    preprocessor = ColumnTransformer([
+        ("num", num_pipeline, num_features),
+        ("cat", cat_pipeline, cat_features)
+    ])
+
+    pipeline = Pipeline([
+        ("preprocessing", preprocessor),
+        ("model", model)
+    ])
+
+    return pipeline
 
 def main():
     df = pd.read_csv(filepath_or_buffer="seattle_weather_prepared.csv")
@@ -11,6 +41,11 @@ def main():
     df_pipeline = df.drop(columns="weather")
     X = df.drop(columns="weather")
     y = df["weather"]
+
+    num_features, cat_features = extract_features(df=df_pipeline)
+    X_train, X_test, y_train, y_test = split_data(X=X, y=y)
+
+
 
 
 
