9938b1781eeb17f32b7d914daf1b9b8beb38a6d5
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-20 14:48:19 +0400

    extreme gradient boosting. initial commit

---
+++ Seattle Weather Forecasting/xg_boosting.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+from features import extract_features, split_data
+from evaluate import evaluate_classification
+
+from xgboost import XGBClassifier
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
