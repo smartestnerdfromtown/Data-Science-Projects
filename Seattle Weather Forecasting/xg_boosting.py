32a8415922414300e36753c2bca52e429cc77bd1
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-20 15:09:39 +0400

    extreme gradient boosting. random searcj

--- Seattle Weather Forecasting/xg_boosting.py
+++ Seattle Weather Forecasting/xg_boosting.py
@@ -29,16 +29,33 @@ def extreme_gradient_boosting_pipeline(model, num_features: list, cat_features: 
 
     pipeline = Pipeline([
         ("preprocessing", preprocessor),
-        ("model", model)
+        ("xgb", model)
     ])
 
     return pipeline
 
+def extreme_gradient_boosting_tuning(
+        param_dist: dict, 
+        xgb_pipeline, 
+        scoring: str, 
+        random_state: int
+    ):
+    search = RandomizedSearchCV(
+        estimator=xgb_pipeline,
+        param_distributions=param_dist,
+        n_iter=20,
+        cv=5,
+        scoring=scoring,
+        n_jobs=-1,
+        random_state=random_state
+    )
+
+    return search
 
 def main():
     df = pd.read_csv(filepath_or_buffer="seattle_weather_prepared.csv")
     df = df.drop(columns="date")
-    
+
     df_pipeline = df.drop(columns=["weather", "weather_encoded"])
     X = df.drop(columns=["weather", "weather_encoded"])
     y = df["weather_encoded"]
@@ -55,7 +72,8 @@ def main():
         reg_alpha=0.1,
         reg_lambda=1.0,
         random_state=42,
-        eval_metric="logloss"
+        eval_metric="mlogloss",
+        objective="multi:softprob"
     )
 
     xgb_pipeline = extreme_gradient_boosting_pipeline(
@@ -74,6 +92,44 @@ def main():
         return_dict=True,
         verbose=True
     )
+
+    for metric, value in evaluation_metrics.items():
+        print(f"{metric}: {value}")
+
+    param_dist = {
+        "xgb__n_estimators": [100, 200, 300],
+        "xgb__learning_rate": [0.01, 0.05, 0.1],
+        "xgb__max_depth": [3, 4, 5],
+        "xgb__min_child_weight": [1, 3, 5],
+        "xgb__gamma": [0, 0.1, 0.3],
+        "xgb__subsample": [0.6, 0.8, 1.0],
+        "xgb__colsample_bytree": [0.6, 0.8, 1.0],
+    }
+        
+    xgb_pipeline = extreme_gradient_boosting_pipeline(
+        model=xgb_model,
+        num_features=num_features,
+        cat_features=cat_features
+    )
+    xgb_tuned = extreme_gradient_boosting_tuning(
+        param_dist=param_dist,
+        xgb_pipeline=xgb_pipeline,
+        scoring="accuracy",
+        random_state=777
+    )
+    xgb_tuned.fit(X_train, y_train)
+    xgb_best_model = xgb_tuned.best_estimator_
+
+    evaluation_metrics = evaluate_classification(
+        model=xgb_best_model,
+        X_test=X_test,
+        y_test=y_test,
+        average="weighted",
+        return_dict=True,
+        verbose=True
+    )
+
+    print("-" * 20)
 
     for metric, value in evaluation_metrics.items():
         print(f"{metric}: {value}")
