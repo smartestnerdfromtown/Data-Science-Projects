54d4304c35cc71f9ef8b54fff0aa1ef119c9fd1d
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-21 07:02:12 +0400

    knn classifier. tuning knn

--- Seattle Weather Forecasting/knn_classifier.py
+++ Seattle Weather Forecasting/knn_classifier.py
@@ -9,6 +9,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import RandomizedSearchCV
 
 
 def create_knn_pipeline(model, num_features: list, cat_features: list):
@@ -33,6 +34,24 @@ def create_knn_pipeline(model, num_features: list, cat_features: list):
     ])
 
     return pipeline
+
+def knn_tuning(
+        param_dist: dict, 
+        knn_pipeline, 
+        scoring: str = "accuracy", 
+        random_state: int = 777
+    ):
+    search = RandomizedSearchCV(
+        estimator=knn_pipeline,
+        param_distributions=param_dist,
+        n_iter=10,
+        cv=5,
+        scoring=scoring,
+        n_jobs=-1,
+        random_state=random_state
+    )
+
+    return search
 
 def main():
     df = pd.read_csv(filepath_or_buffer="seattle_weather_prepared.csv")
@@ -72,6 +91,41 @@ def main():
     for metric, value in evaluation_metrics.items():
         print(f"{metric}: {value}")
 
+    param_dist = {
+        "model__n_neighbors": [3, 5, 7, 9, 11],
+        "model__weights": ["uniform", "distance"],
+        "model__p": [1, 2]
+    }
+
+    knn_pipeline = create_knn_pipeline(
+        model=knn_model,
+        num_features=num_features,
+        cat_features=cat_features
+    )
+
+    knn_tuned = knn_tuning(
+        param_dist=param_dist,
+        knn_pipeline=knn_pipeline,
+        scoring="f1_weighted",
+        random_state=777
+    )
+    knn_tuned.fit(X_train, y_train)
+
+    knn_best_model = knn_tuned.best_estimator_
+
+    evaluation_metrics = evaluate_classification(
+        model=knn_best_model,
+        X_test=X_test,
+        y_test=y_test,
+        average="weighted",
+        return_dict=True,
+        verbose=True
+    )
+
+    print("-" * 20)
+
+    for metric, value in evaluation_metrics.items():
+        print(f"{metric}: {value}")
 
 if __name__ == "__main__":
     main()
