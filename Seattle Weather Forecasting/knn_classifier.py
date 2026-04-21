44e905a30c3f54dc20e682157ff886b312ec8551
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-21 06:55:23 +0400

    knn classifier. basic models

--- Seattle Weather Forecasting/knn_classifier.py
+++ Seattle Weather Forecasting/knn_classifier.py
@@ -8,9 +8,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
+from sklearn.neighbors import KNeighborsClassifier
 
 
-def knn_pipeline(model, num_features: list, cat_features: list):
+def create_knn_pipeline(model, num_features: list, cat_features: list):
     num_pipeline = Pipeline([
         ("imputer", SimpleImputer(strategy="median")),
         ("scaler", StandardScaler())
@@ -34,7 +35,43 @@ def knn_pipeline(model, num_features: list, cat_features: list):
     return pipeline
 
 def main():
-    pass
+    df = pd.read_csv(filepath_or_buffer="seattle_weather_prepared.csv")
+    df = df.drop(columns="date")
+
+    df_pipeline = df.drop(columns=["weather", "weather_encoded"])
+    X = df.drop(columns=["weather", "weather_encoded"])
+    y = df["weather_encoded"]
+
+    num_features, cat_features = extract_features(df=df_pipeline)
+    X_train, X_test, y_train, y_test = split_data(X=X, y=y)
+
+    knn_model = KNeighborsClassifier(
+        n_neighbors=5,
+        weights="distance",
+        metric="minkowski",
+        p=2
+    )
+
+    knn_pipeline = create_knn_pipeline(
+        model=knn_model,
+        num_features=num_features,
+        cat_features=cat_features
+    )
+
+    knn_pipeline.fit(X_train, y_train)
+
+    evaluation_metrics = evaluate_classification(
+        model=knn_pipeline,
+        X_test=X_test,
+        y_test=y_test,
+        average="weighted",
+        return_dict=True,
+        verbose=True
+    )
+
+    for metric, value in evaluation_metrics.items():
+        print(f"{metric}: {value}")
+
 
 if __name__ == "__main__":
     main()
