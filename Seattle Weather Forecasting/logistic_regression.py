5b71d4e822c73dc843ce61b9b04f65bcc525abe3
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-21 07:07:07 +0400

    logistic regression. initial commit

---
+++ Seattle Weather Forecasting/logistic_regression.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+from features import extract_features, split_data
+from evaluate import evaluate_classification
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
