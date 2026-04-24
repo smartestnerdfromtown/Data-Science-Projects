b01949b54f4a17e509ae51bd6326554acdc17735
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-24 07:51:00 +0400

    Ada Boosting Classifier. basic model

--- Seattle Weather Forecasting/ada_boosting_classifier.py
+++ Seattle Weather Forecasting/ada_boosting_classifier.py
@@ -17,6 +17,42 @@ def main():
     num_features, cat_features = extract_features(df=df_pipeline)
     X_train, X_test, y_train, y_test = split_data(X=X, y=y)
 
+    model = AdaBoostClassifier(
+        estimator=None,  # If None, then the base estimator is DecisionTreeClassifier 
+                         # initialized with max_depth=1.
+        n_estimators=50, 
+        learning_rate=1.0, 
+        random_state=None
+    )
+
+    pipeline = create_pipeline(
+        model=model,
+        num_features=num_features,
+        cat_features=cat_features,
+        scaler=StandardScaler(),
+        encoder=OneHotEncoder()
+    )
+
+    pipeline.fit(X_train, y_train)
+
+    evaluation_metrics = evaluate_classification(
+        model=pipeline,
+        X_test=X_test,
+        y_test=y_test,
+        average="weighted",
+        include_cm=False,
+        include_roc_auc=False,
+        return_dict=True,
+        verbose=True
+    )
+
+    show_results(results=evaluation_metrics)
+
+    save_evaluation_metrics(
+        file_to_csv="models_metrics.csv", 
+        model_name="ada_boosting_classifier",
+        results=evaluation_metrics
+    )
 
 if __name__ == "__main__":
     main()
