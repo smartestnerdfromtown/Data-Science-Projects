caf326194abafa37689e2239ae6737913768695f
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-27 07:57:45 +0400

    Nearest Centroid. base model

--- Seattle Weather Forecasting/nearest_centroid.py
+++ Seattle Weather Forecasting/nearest_centroid.py
@@ -13,8 +13,51 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.neighbors import NearestCentroid
 
+
+RANDOM_STATE = 777
+N_JOBS = -1
+
+
 def main():
-    pass
+    df_pipeline, X, y = prepare_data(file_path="seattle_weather_prepared.csv")
+    num_features, cat_features = extract_features(df=df_pipeline)
+    X_train, X_test, y_train, y_test = split_data(X=X, y=y)
+
+    model = NearestCentroid(
+        metric='euclidean', 
+        shrink_threshold=None, 
+        priors='uniform'
+    )
+
+    pipeline = create_pipeline(
+        model=model,
+        num_features=num_features,
+        cat_features=cat_features,
+        scaler=StandardScaler(),
+        encoder=OneHotEncoder()
+    )
+
+    pipeline.fit(X_train, y_train)
+
+    evaluation_metrics = evaluate_classification(
+        model=pipeline,
+        X_test=X_test,
+        y_test=y_test,
+        average="weighted",
+        include_cm=False,
+        include_roc_auc=False,
+        return_dict=True,
+        verbose=True
+    )
+
+    show_results(results=evaluation_metrics)
+
+    save_evaluation_metrics(
+        file_to_csv="models_metrics.csv", 
+        model_name="nearest_centroid_classifier",
+        results=evaluation_metrics
+    )
+
 
 if __name__ == "__main__":
     main()
