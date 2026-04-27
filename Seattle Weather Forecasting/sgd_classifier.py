964959ee3060b2c1b4a9507b819a54c06df9cef0
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-27 07:42:32 +0400

    SGD Classifier. basic model

--- Seattle Weather Forecasting/sgd_classifier.py
+++ Seattle Weather Forecasting/sgd_classifier.py
@@ -13,8 +13,68 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import SGDClassifier
 
+
+RANDOM_STATE = 777
+N_JOBS = -1
+
+
 def main():
-    pass
+    df_pipeline, X, y = prepare_data(file_path="seattle_weather_prepared.csv")
+    num_features, cat_features = extract_features(df=df_pipeline)
+    X_train, X_test, y_train, y_test = split_data(X=X, y=y)
+
+    model = SGDClassifier(
+        loss='hinge', 
+        penalty='l2', 
+        alpha=0.0001, 
+        l1_ratio=0.15, 
+        fit_intercept=True, 
+        max_iter=1000, 
+        tol=0.001, 
+        shuffle=True, 
+        verbose=0, 
+        epsilon=0.1, 
+        n_jobs=N_JOBS, 
+        random_state=RANDOM_STATE, 
+        learning_rate='optimal', 
+        eta0=0.01, 
+        power_t=0.5, 
+        early_stopping=False, 
+        validation_fraction=0.1, 
+        n_iter_no_change=5, 
+        class_weight=None, 
+        warm_start=False, 
+        average=False
+    )
+
+    pipeline = create_pipeline(
+        model=model,
+        num_features=num_features,
+        cat_features=cat_features,
+        scaler=StandardScaler(),
+        encoder=OneHotEncoder()
+    )
+
+    pipeline.fit(X_train, y_train)
+
+    evaluation_metrics = evaluate_classification(
+        model=pipeline,
+        X_test=X_test,
+        y_test=y_test,
+        average="weighted",
+        include_cm=False,
+        include_roc_auc=False,
+        return_dict=True,
+        verbose=True
+    )
+
+    show_results(results=evaluation_metrics)
+
+    save_evaluation_metrics(
+        file_to_csv="models_metrics.csv", 
+        model_name="sgd_classifier",
+        results=evaluation_metrics
+    )
 
 if __name__ == "__main__":
     main()
