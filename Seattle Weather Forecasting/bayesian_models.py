357cd22f0d478b7f72ec3742a483c46ef76365e3
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-28 08:25:00 +0400

    Bayesian Models. GaussianNB

--- Seattle Weather Forecasting/bayesian_models.py
+++ Seattle Weather Forecasting/bayesian_models.py
@@ -55,7 +55,42 @@ def main():
         results=evaluation_metrics
     )
 
-    
+
+    ############ GAUSSIAN NB ############ 
+
+
+    model = GaussianNB(priors=None, var_smoothing=1e-09)
+
+    pipeline = create_pipeline(
+        model=model,
+        num_features=num_features,
+        cat_features=cat_features,
+        scaler=StandardScaler(),
+        encoder=OneHotEncoder()
+    )
+
+    pipeline.fit(X_train, y_train)
+
+    evaluation_metrics = evaluate_classification(
+        model=pipeline,
+        X_test=X_test,
+        y_test=y_test,
+        average="weighted",
+        include_cm=False,
+        include_roc_auc=False,
+        return_dict=True,
+        verbose=True
+    )
+
+    show_results(results=evaluation_metrics)
+
+    save_evaluation_metrics(
+        file_to_csv="models_metrics.csv", 
+        model_name="gaussian_nb_classifier",
+        results=evaluation_metrics
+    )
+
+
 
 
 
