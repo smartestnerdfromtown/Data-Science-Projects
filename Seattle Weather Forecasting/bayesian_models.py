57d1130895a82ca726bbdcae6e71c302d7833f9b
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-28 08:30:20 +0400

    Bayesian Models. final

--- Seattle Weather Forecasting/bayesian_models.py
+++ Seattle Weather Forecasting/bayesian_models.py
@@ -5,18 +5,18 @@ from features import extract_features, split_data, prepare_data
 from evaluate import evaluate_classification, save_evaluation_metrics, show_results
 from define_pipeline import create_pipeline, tuning
 
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
+from sklearn.naive_bayes import GaussianNB, BernoulliNB
 
 
 def main():
     df_pipeline, X, y = prepare_data(file_path="seattle_weather_prepared.csv")
     num_features, cat_features = extract_features(df=df_pipeline)
     X_train, X_test, y_train, y_test = split_data(X=X, y=y)
+
+
+    ############ BERNUOLLI NB ############ 
+
 
     model = BernoulliNB(
         alpha=1.0, 
@@ -89,9 +89,6 @@ def main():
         model_name="gaussian_nb_classifier",
         results=evaluation_metrics
     )
-
-
-
 
 
 if __name__ == "__main__":
