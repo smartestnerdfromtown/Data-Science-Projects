f30c653baa7e6ba4f18676b527f346df1a403062
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-04-28 08:20:24 +0400

    Bayesian Models.

---
+++ Seattle Weather Forecasting/bayesian_models.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+from features import extract_features, split_data, prepare_data
+from evaluate import evaluate_classification, save_evaluation_metrics, show_results
+from define_pipeline import create_pipeline, tuning
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
