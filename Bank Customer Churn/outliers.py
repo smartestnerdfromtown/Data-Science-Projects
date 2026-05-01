6423e14b08c3481a8d3bb887f85f5188d25ba0b4
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-05-01 08:27:48 +0400

    outliers. isolaton forest

--- Bank Customer Churn/outliers.py
+++ Bank Customer Churn/outliers.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
+
+
+RANDOM_STATE = 777
+
+
 def z_score(df: pd.DataFrame, column_name: str) -> pd.Series:
     mean = df[column_name].mean()
     std = df[column_name].std()
@@ -20,5 +27,27 @@ def calculate_iqr(df: pd.DataFrame, column_name: str) -> pd.Series:
 
     return IQR, lower_bound, upper_bound
 
+
+def find_outliers_via_forest(df: pd.DataFrame, numerical_cols: list, to_scale: bool) -> pd.Series:
+    X = df.loc[:, numerical_cols]
+    
+    if to_scale:
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
+
+
+    model = IsolationForest(
+        contamination="auto",
+        random_state=RANDOM_STATE,
+        n_estimators=200,     
+        max_samples="auto"
+    )
+
+    model.fit(X)
+
+    scores = model.decision_function(X)
+    labels = model.predict(X)  # -1 = anomaly, 1 = normal
+
+    return labels
 
 
